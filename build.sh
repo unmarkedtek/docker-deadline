78eb3ce4901b6013de32cf4ee744d83f5728e71a
George Siddiqui <george@blue-bolt.com>
2019-12-29 14:14:48 +0000

    using maya2019 and centos7

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+docker build -f maya2019-base-centos7.Dockerfile -t maya2019-base-centos7 .
 docker build -f deadline10.Dockerfile -t deadline10-client .
-docker build -f maya2020.Dockerfile -t deadline-client-maya2020 .
+docker build -f maya2019.Dockerfile -t deadline-client-maya2019 .
+#docker build -f maya2020.Dockerfile -t deadline-client-maya2020 .
