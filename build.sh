4dff58f5712b19c98e3fe3871d3b292df653d641
George Siddiqui <george@blue-bolt.com>
2019-12-29 23:22:06 +0000

    replace maya base with centos-base-server and conformed DockerFile names

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-docker build -f maya2019-base-centos7.Dockerfile -t maya2019-base-centos7 .
-docker build -f deadline10.Dockerfile -t deadline10-client .
-docker build -f maya2019.Dockerfile -t deadline-client-maya2019 .
+
+docker build -f centos7-base-server.Dockerfile -t centos7-base-server .
+docker build -f deadline10-client.Dockerfile -t deadline10-client .
+docker build -f deadline10-client-maya2019.Dockerfile -t deadline10-client-maya2019 .
 
 # for newer deadline with maya 2020 
 #docker build -f maya2020.Dockerfile -t deadline-client-maya2020 .
