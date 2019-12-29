53ddb4cd2593f68c538081622def8eb727730dea
George Siddiqui <george@blue-bolt.com>
2019-12-29 13:55:26 +0000

    get host ip address and use deadline-client-maya2019

--- run.sh
+++ run.sh
@@ -1,12 +1,33 @@
+#
+# run.sh is used by deadlinemonitor and deadlinewebservice
+#
+
+# Add docker host ip to access control list 
+if [[ $(uname -s) == "Darwin" ]]; then
+	ip=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2)
+elif [[ $(uname -s) == "Linux" ]]; then
+	ip=$(hostname -i)
+else
+	exit "unknown host os"
+fi
+xhost + $ip
+
+
+
 NAME=$1
 EXECUTABLE=$2
 
 docker run -ti --rm \
 	--name $NAME \
 	-h $NAME \
-	-v deadline-volume:/mnt/DeadlineRepository8 \
-	-v $(pwd)/share:/share \
-	-e DISPLAY=192.168.1.212:0 \
-	--link deadline-mongo:mongo \
+    -e DISPLAY=$ip:0 \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+  	-v deadline-volume:/mnt/DeadlineRepository10 \
+	-v /Volumes/disk1/george-s/playpen/dev/docker-deadline/share:/share  \
+	--network="docker-deadline_default" \
 	--entrypoint $EXECUTABLE \
-	deadline-client-maya
+	deadline-client-maya2019
+
+
+docker network connnect docker-deadline_default $NAME
+#docker run -d --name firefox -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix jess/firefox
