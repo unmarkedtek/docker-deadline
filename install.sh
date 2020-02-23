95d94bab2b91992408c50bf2cd1d2f28f5b4d75c
George Heheki <geeee@lelo.com>
2020-02-23 20:45:34 +0000

    make the installer.sh work for mac and linux paths

--- install.sh
+++ install.sh
@@ -2,8 +2,17 @@
 #
 # Copy the Deadline Repository into an internal Docker volume
 #
+
+# Set volumes for OSX and Linux differently - modify this for your own
+if [[ $(uname -s) == "Darwin" ]]; then
+	installers_volume="/Volumes/disk1/george-s/playpen/dev/docker-deadline/installers:/installers"
+elif [[ $(uname -s) == "Linux" ]]; then
+	installers_volume="/disk1/george-s/playpen/dev/docker-deadline/installers:/installers"
+else
+	exit "unknown host os"
+fi
 docker run -ti --rm \
-	-v /Volumes/disk1/george-s/playpen/dev/docker-deadline/installers:/installers \
+	-v $installers_volume:/installers \
 	-v deadline-volume:/installdir \
 	-w /installers \
 	centos:7 bash -c "
