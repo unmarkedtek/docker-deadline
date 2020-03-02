9d6130eea88ab9bc7943fa0200884c537a941df8
George Siddiqui <george-s@peterson.blue-bolt.lan>
2020-03-02 22:21:01 +0000

    samba works on linux+mac + shares installers

--- samba.sh
+++ samba.sh
@@ -1,32 +1,17 @@
 #!/usr/bin/env bash
 
-
 docker network create --driver bridge --ipv6 --subnet fd15:555::/64 --subnet 172.16.238.0/24 docker-deadline_dockernet  --attachable
 
+# Set volumes for OSX and Linux differently - modify this for your own
+if [[ $(uname -s) == "Darwin" ]]; then
+	installers_volume="/Volumes/disk1/george-s/playpen/dev/docker-deadline/installers:/installers"
+elif [[ $(uname -s) == "Linux" ]]; then
+	installers_volume="/disk1/george-s/playpen/dev/docker-deadline/installers:/installers"
+else
+	exit "unknown host os"
+fi
 
-# docker run \
-# 	-ti \
-# 	--rm \
-# 	-p 139:139 \
-# 	-p 445:445 \
-# 	--name deadline-samba \
-# 	-v deadline-volume:/share \
-# 	-d dperson/samba -p \
-# 	-u "example;badpass" \
-# 	-w "DEV"
-# 	-s "users;/srv;no;no;no;example" \
-# 	-s "DeadlineRepository10;/share;yes;no;yes;all;none;all" \
-		
-# docker run -it -p 139:139 -p 445:445 --name deadline-samba -d dperson/samba -p \
-# 			-w "DEV" \
-# 			-u "example1;badpass" \
-#             -u "example2;badpass" \
-#             -s "DeadlineRepository10;/share;yes;no;yes;all;none;all" \
-# 			-s "public;/share" \
-#             -s "users;/srv;no;no;no;example1,example2" \
-#             -s "example1 private share;/example1;no;no;no;example1" \
-#             -s "example2 private share;/example2;no;no;no;example2"
-
+# Share repository and installers folder via samba server - expose port on docker host
 docker run \
 	-ti \
 	--rm \
@@ -34,5 +19,9 @@ docker run \
 	-p 445:445 \
 	--name deadline-samba \
 	-v deadline-volume:/share \
+	-v ${installers_volume} \
 	-d dperson/samba -p \
 	-s "DeadlineRepository10;/share;yes;no;yes;all;none;all" \
+	-s "Deadline10installers;/installers;yes;no;yes;all;none;all" \
+
+#-s "DeadlineRepository10;/share;yes;no;yes;all;none;all" \
