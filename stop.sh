4c5bc200424d7e961e935c12a9fce034445ef906
George Siddiqui <george-s@peterson.blue-bolt.lan>
2020-03-04 23:51:15 +0000

    stop doesn't delete containers

---
+++ stop.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+pushd `dirname $0` > /dev/null
+DIRNAME=`pwd -P`
+
+# This directory is available from within each Deadline slave
+export SHARE=$DIRNAME/share
+
+# Default image used with Docker Compose, defaults to a vanilla session
+# You can override the image by setting an environment variable.
+export IMAGE=${IMAGE:-deadline-client-maya2017}
+
+docker-compose stop
+
+popd > /dev/null
