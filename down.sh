a67a6adaec0f0a11bb01da45995bb08321ad18e7
George Heheki <geeee@lelo.com>
2020-03-06 21:12:36 +0000

    made docker compose and up.sh mac compatible

--- down.sh
+++ down.sh
@@ -9,6 +9,25 @@ export SHARE=$DIRNAME/share
 # You can override the image by setting an environment variable.
 export IMAGE=${IMAGE:-deadline-client-maya2017}
 
+# Set installer path for OSX and Linux differently
+if [[ $(uname -s) == "Darwin" ]]; then
+	export DOCKER_HOST_DOCKER_DEADLINE_INSTALLER_PATH="/Volumes/disk1/george-s/playpen/dev/docker-deadline/installers"
+elif [[ $(uname -s) == "Linux" ]]; then
+	export DOCKER_HOST_DOCKER_DEADLINE_INSTALLER_PATH="/disk1/george-s/playpen/dev/docker-deadline/installers"
+else
+	exit "unknown host os"
+fi
+
+
+# Set scripts path for OSX and Linux differently
+if [[ $(uname -s) == "Darwin" ]]; then
+	export DOCKER_HOST_DOCKER_DEADLINE_SCRIPTS_PATH="/Volumes/disk1/george-s/playpen/dev/docker-deadline/scripts"
+elif [[ $(uname -s) == "Linux" ]]; then
+	export DOCKER_HOST_DOCKER_DEADLINE_SCRIPTS_PATH="/disk1/george-s/playpen/dev/docker-deadline/scripts"
+else
+	exit "unknown host os"
+fi
+
 docker-compose down
 
 popd > /dev/null
--- up.sh
+++ up.sh
@@ -16,14 +16,28 @@ export IMAGE=${IMAGE:-deadline10-client}
 # Workaround to start ipv6 network in a docker compose like way
 # https://stackoverflow.com/questions/55737196/how-to-enable-ipv6-in-docker-compose-version-3
 
+
 docker network create --driver bridge --ipv6 --subnet fd15:555::/64 --subnet 172.16.238.0/24 docker-deadline_dockernet  --attachable
 
+# Set installer path for OSX and Linux differently
+if [[ $(uname -s) == "Darwin" ]]; then
+	export DOCKER_HOST_DOCKER_DEADLINE_INSTALLER_PATH="/Volumes/disk1/george-s/playpen/dev/docker-deadline/installers"
+elif [[ $(uname -s) == "Linux" ]]; then
+	export DOCKER_HOST_DOCKER_DEADLINE_INSTALLER_PATH="/disk1/george-s/playpen/dev/docker-deadline/installers"
+else
+	exit "unknown host os"
+fi
+
+
+# Set scripts path for OSX and Linux differently
+if [[ $(uname -s) == "Darwin" ]]; then
+	export DOCKER_HOST_DOCKER_DEADLINE_SCRIPTS_PATH="/Volumes/disk1/george-s/playpen/dev/docker-deadline/scripts"
+elif [[ $(uname -s) == "Linux" ]]; then
+	export DOCKER_HOST_DOCKER_DEADLINE_SCRIPTS_PATH="/disk1/george-s/playpen/dev/docker-deadline/scripts"
+else
+	exit "unknown host os"
+fi
+
 docker-compose up
-
-sleep 30
-docker-compose exec deadline-slave1 /opt/Thinkbox/Deadline10/bin/deadlinecommand addpool spot
-docker-compose exec deadline-slave1 /opt/Thinkbox/Deadline10/bin/deadlinecommand addgroup spot-fleet
-docker-compose exec deadline-slave1 /opt/Thinkbox/Deadline10/bin/deadlinecommand AddPoolToSlave maya-slave1 spot
-
 
 popd > /dev/null
